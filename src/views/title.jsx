725940931ee2f98abfb3f60f4907652af4bd39a2
Lisheri <496585582@qq.com>
2021-01-20 21:30:43 +0800

    去掉不必要的注释

--- src/views/title.jsx
+++ src/views/title.jsx
@@ -9,15 +9,14 @@ class Title extends React.Component {
         }
     }
     handleClick = () => {
-        // this.state.isEdit = !this.state.isEdit
         this.setState({
             isEdit: !this.state.isEdit
         })
     }
 
     render() {
-        return (<div style={{textAlign: 'left'}}>
-            {!this.state.isEdit ? <span style={{width: '100px'}}>{this.props.title}</span> : <Input style={{width: '100px'}} value={this.props.title} onChange={this.props.handleTitleChange}/> }
+        return (<div style={{ textAlign: 'left' }}>
+            {!this.state.isEdit ? <span style={{ width: '100px' }}>{this.props.title}</span> : <Input style={{ width: '100px' }} value={this.props.title} onChange={this.props.handleTitleChange} />}
             <Button onClick={this.handleClick}>{this.state.isEdit ? '确认' : '修改'}</Button>
         </div >)
     }
